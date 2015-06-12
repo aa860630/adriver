69748ba153ac70a992be88b7957a71b5cc2b224b
Jongy <yon.goldschmidt@gmail.com>
2015-06-12 20:43:40 +0300

    add print_sfilter for debugging; remove x perms from files

--- sfilter.c
+++ sfilter.c
@@ -1,10 +1,24 @@
 // runs our filters on packet data
 
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
 #include <linux/kernel.h>
 #include <linux/string.h>
 
 #include "defs.h"
 
+
+static void print_sfilter(struct sfilter *sf)
+{
+    int i = 0;
+
+    pr_debug("sfilter matches: ");
+    for (i = 0; i < sf->num_matches; ++i)
+    {
+        pr_debug("'%s', ", sf->matches[i]);
+    }
+    pr_debug("\n");
+}
 
 static bool run_sfilter(struct sfilter *sf, char *s)
 {
@@ -26,6 +40,7 @@ static bool run_sfilter(struct sfilter *sf, char *s)
         last += strlen(match);
     }
 
+    print_sfilter(sf); // for debugging
     return true;
 }
 
